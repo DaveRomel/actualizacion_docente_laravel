cb3ef7b038a75fc9f172c97c0d8b3f7b1e90a21e
jjvdc22003 <jjvdc22003@gmail.com>
2025-05-22 16:50:56 -0600

    Registro y Editar como layouts y actualización de welcome e index

---
+++ resources/views/actualizacion_docente/layouts/editar.blade.php
@@ -0,0 +1,44 @@
+@extends('actualizacion_docente.welcome')
+<!-- 
+COSAS QUE VARIAN PERO NO SE PUEDEN CAMBIAR:
+head:
+    <title>Actualizar Información</title>
+body:
+    <header>
+        <nav>
+            <div><a class="barra-nav" href="#"><u>Inicio</u></a></div>
+        </nav>
+    </header>
+-->
+@section('contenido')
+
+<div class="contenedor-formulario">
+    <img src="{{ asset('images/registro.png') }}" alt="Icono editar" style="width: 80px; height: 80px;">
+    <div>
+        <div class="titulo-registro"><strong>Editar información</strong></div>
+    </div>
+    <form action="#" method="PUT">
+        @csrf
+
+        <div class="form-group">
+            <input type="text" name="nombre" placeholder="Nombre" required>
+        </div>
+
+        <div class="form-group">
+            <input type="tel" name="telefono" placeholder="Número de teléfono" required>
+        </div>
+
+        <div class="form-group">
+            <input type="text" name="escuela" placeholder="Escuela de procedencia" required>
+        </div>
+
+        <div class="form-group">
+            <input type="email" name="correo" placeholder="Correo electrónico" required>
+        </div>
+
+        <button type="submit" class="btn-registrarme">Guardar cambios</button>
+    </form>
+</div>
+
+@endsection
+    
