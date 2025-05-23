4c66103795f1b7921b028b73efc6a10dfa678908
saulpena636 <suoerpenniya@gmail.com>
2025-05-23 16:26:58 -0600

    Feat: apis del Controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FastApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('/crear-usuario', [FastApiController::class, 'createUser']);
+Route::put('/actualizar-usuario/{user_id}', [FastApiController::class, 'updateUser']);
+Route::put('/cambiar-estado-usuario/{status}/{user_id}', [FastApiController::class, 'changeUserStatus']);
+Route::post('/recuperar-password', [FastApiController::class, 'recuperarPassword']);
+Route::put('/cambiar-contrasena', [FastApiController::class, 'cambiarContrasena']);
+Route::post('/inscribir-usuario/{usuario_id}/{materia_id}', [FastApiController::class, 'inscribirUsuario']);
+Route::get('/ver-inscripciones/{materia_id}', [FastApiController::class, 'verInscripcionesPorMateria']);
+Route::get('/contar-inscritos/{materia_id}', [FastApiController::class, 'contarInscritos']);
+Route::delete('/eliminar-inscripcion/{usuario_id}', [FastApiController::class, 'eliminarInscripcion']);
+Route::post('/login', [FastApiController::class, 'login']);
+Route::get('/usuario-actual', [FastApiController::class, 'getCurrentUser']);
+Route::get('/usuario-items', [FastApiController::class, 'getUserItems']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
